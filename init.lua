17c0729900c6b37017675c3ee2fbd590764f418f
vaibhav-nayak18 <vaibhavnayak018@gmail.com>
2023-01-28 20:55:28 +0530

    Started configuring  neovim

---
+++ init.lua
@@ -0,0 +1,4 @@
+require('plugins')
+
+vim.cmd('colorscheme carbonfox')
+
---
+++ lua/plugins/init.lua
@@ -0,0 +1,16 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use "EdenEast/nightfox.nvim"
+  use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  },
+  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+}
+  use {'nvim-treesitter/nvim-treesitter' }
+  
+  end)
